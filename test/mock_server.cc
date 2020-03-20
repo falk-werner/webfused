35122136fd4b10bd04ae9143f0b99f342147c999
Falk Werner <falk.werner@gmx.net>
2020-03-20 11:32:45 +0100

    added daemon test

---
+++ test/mock_server.cc
@@ -0,0 +1,25 @@
+#include "mock_server.hpp"
+#include "wrap_utils.hpp"
+
+extern "C"
+{
+static webfused_test::IServer * wfd_MockServer = nullptr;
+
+WFD_WRAP_FUNC1(wfd_MockServer, struct wf_server *, wf_server_create, struct wf_server_config *);
+
+}
+
+namespace webfused_test
+{
+
+MockServer::MockServer()
+{
+    wfd_MockServer = this;
+}
+
+MockServer::~MockServer()
+{
+    wfd_MockServer = nullptr;
+}
+
+}
