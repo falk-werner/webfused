c972c17bd231a059920bdcad48b1e8f568622b99
Falk Werner <falk.werner@gmx.net>
2020-10-14 22:14:27 +0200

    updated libfuse to 3.10.0, libwebsockets to 4.1.3, libjansson to 2.13.1

--- deps.sh
+++ deps.sh
@@ -1,20 +1,20 @@
 PACKAGES="libfuse lws jansson gtest webfuse"
 
-libfuse_VERSION=3.9.1
+libfuse_VERSION=3.10.0
 libfuse_URL=https://github.com/libfuse/libfuse/archive/fuse-${libfuse_VERSION}.tar.gz
-libfuse_MD5=5f7c1062def710d8b60343524a18cc82
+libfuse_MD5=22aec9bc9008eea6b17e203653d1b938
 libfuse_DIR=libfuse-fuse-${libfuse_VERSION}
 libfuse_TYPE=meson
 
-lws_VERSION=4.0.13
+lws_VERSION=4.1.3
 lws_URL=https://github.com/warmcat/libwebsockets/archive/v${lws_VERSION}.tar.gz
-lws_MD5=6403448e26d302501969afb0e128143b
+lws_MD5=413cbe790ccb089001f53b2ee167b9c2
 lws_DIR=libwebsockets-${lws_VERSION}
 lws_TYPE=cmake
 
-jansson_VERSION=2.12
+jansson_VERSION=2.13.1
 jansson_URL=https://github.com/akheron/jansson/archive/v${jansson_VERSION}.tar.gz
-jansson_MD5=c4b106528d5ffb521178565de1ba950d
+jansson_MD5=3d589a62053874893715453a46a32a0e
 jansson_DIR=jansson-${jansson_VERSION}
 jansson_TYPE=cmake
 
