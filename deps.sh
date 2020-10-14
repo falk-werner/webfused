6e62076d552c32e4123b5b6ab89ba3008bb9f9e3
Falk Werner <falk.werner@gmx.net>
2020-10-14 20:10:25 +0200

    updated libwebsockets to 4.0.13

--- deps.sh
+++ deps.sh
@@ -6,9 +6,9 @@ libfuse_MD5=5f7c1062def710d8b60343524a18cc82
 libfuse_DIR=libfuse-fuse-${libfuse_VERSION}
 libfuse_TYPE=meson
 
-lws_VERSION=3.2.0
+lws_VERSION=4.0.13
 lws_URL=https://github.com/warmcat/libwebsockets/archive/v${lws_VERSION}.tar.gz
-lws_MD5=1d06f5602604e67e6f50cef9857c6b0c
+lws_MD5=6403448e26d302501969afb0e128143b
 lws_DIR=libwebsockets-${lws_VERSION}
 lws_TYPE=cmake
 
