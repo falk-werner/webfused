e1705d50cf11cbab100be3476a7bebd220eec7cf
Falk Werner <falk.werner@gmx.net>
2020-10-14 19:48:53 +0200

    install with sudo

--- install_deps.sh
+++ install_deps.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 
-PACKAGES="libfuse lws jansson"
-
-libfuse_VERSION=3.9.1
-libfuse_URL=https://github.com/libfuse/libfuse/archive/fuse-${libfuse_VERSION}.tar.gz
-libfuse_MD5=5f7c1062def710d8b60343524a18cc82
-libfuse_DIR=libfuse-fuse-${libfuse_VERSION}
-libfuse_TYPE=meson
-
-lws_VERSION=3.2.0
-lws_URL=https://github.com/warmcat/libwebsockets/archive/v${lws_VERSION}.tar.gz
-lws_MD5=1d06f5602604e67e6f50cef9857c6b0c
-lws_DIR=libwebsockets-${lws_VERSION}
-lws_TYPE=cmake
-
-jansson_VERSION=2.12
-jansson_URL=https://github.com/akheron/jansson/archive/v${jansson_VERSION}.tar.gz
-jansson_MD5=c4b106528d5ffb521178565de1ba950d
-jansson_DIR=jansson-${jansson_VERSION}
-jansson_TYPE=cmake
+. deps.sh
 
 set -e
 
@@ -41,7 +23,7 @@ install_package() {
         meson build
         cd build
         ninja
-        ninja install
+        sudo ninja install
         cd ../..
         ;;
     cmake)
@@ -49,7 +31,7 @@ install_package() {
         cd "${!PACKAGE_DIR}/build"
         cmake ..
         make
-        make install
+        sudo make install
         cd ../..
         ;;
     *)
