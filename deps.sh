c71d04dcbe308b62f4ef97b0be1728e42d345f3b
Falk Werner <falk.werner@gmx.net>
2020-10-17 22:56:51 +0200

    updated libwebsockets to 4.1.3; updated webfuse to 0.5.1

--- deps.sh
+++ deps.sh
@@ -6,9 +6,9 @@ libfuse_MD5=22aec9bc9008eea6b17e203653d1b938
 libfuse_DIR=libfuse-fuse-${libfuse_VERSION}
 libfuse_TYPE=meson
 
-lws_VERSION=4.0.13
+lws_VERSION=4.1.3
 lws_URL=https://github.com/warmcat/libwebsockets/archive/v${lws_VERSION}.tar.gz
-lws_MD5=6403448e26d302501969afb0e128143b
+lws_MD5=413cbe790ccb089001f53b2ee167b9c2
 lws_DIR=libwebsockets-${lws_VERSION}
 lws_TYPE=cmake
 
@@ -24,8 +24,8 @@ gtest_MD5=ecd1fa65e7de707cd5c00bdac56022cd
 gtest_DIR=googletest-release-${gtest_VERSION}
 gtest_TYPE=cmake
 
-webfuse_VERSION=0.5.0
+webfuse_VERSION=0.5.1
 webfuse_URL=https://github.com/falk-werner/webfuse/archive/v${webfuse_VERSION}.tar.gz
-webfuse_MD5=535c9f261c90c109a138daa3ee9f95ba
+webfuse_MD5=a2e0e9d24cf7bc2d2273b5c64788adca
 webfuse_DIR=webfuse-${webfuse_VERSION}
 webfuse_TYPE=meson
